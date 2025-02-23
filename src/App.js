d0dce56fdf30aacdf3da13aec5f1105bc12cd1f0
Daniel Velnic <velnicd@yahoo.com>
2025-02-23 20:51:59 +0200

    First commit - initialize the project "React Bootstrap" of channel "Code with Bibek" from youtube.com

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary" >Primary</Button>
+    </div>
+  );
+}
+
+export default App;
